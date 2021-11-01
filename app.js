63f545117fdad242d808029aed8689a8ce8a368b
Ramses Solano <ramses.solano@globant.com>
2021-11-01 13:12:06 -0500

    add methods

--- app.js
+++ app.js
@@ -5,15 +5,47 @@ const port = 3000;
 const app = express();
 
 
-app.use( bodyParser.urlencoded({ extended: true }) );
-app.use(  bodyParser.json() );
-app.use( cors( {origin: /http:\/\/localhost/ } ) );
-app.options( '*', cors() );
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors({ origin: /http:\/\/localhost/ }));
+app.options('*', cors());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send({
+        "success": true,
+        "message": "User logged in successfully",
+        "data": {}
+    })
+
 })
 
+app.post('/login', (req, res) => {
+    res.send({
+        "success": true,
+        "message": "User logged in successfully",
+        "data": {}
+    })
+
+})
+
+app.post('/', (req, res) => {
+    res.send({
+        "success": true,
+        "message": "User logged in successfully",
+        "data": {}
+    })
+
+})
+
+app.put('/', (req, res) => {
+    res.send({
+        "success": true,
+        "message": "User logged in successfully",
+        "data": {}
+    })
+})
+
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
